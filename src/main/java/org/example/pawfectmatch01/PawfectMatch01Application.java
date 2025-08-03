b9900ef5984042b78c84e1555c1024eebb290ae8
sakuni <saakfernandz@gmail.com>
2025-08-03 09:37:09 +0530

    Initial commit

---
+++ src/main/java/org/example/pawfectmatch01/PawfectMatch01Application.java
@@ -0,0 +1,13 @@
+package org.example.pawfectmatch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawfectMatch01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawfectMatch01Application.class, args);
+    }
+
+}
